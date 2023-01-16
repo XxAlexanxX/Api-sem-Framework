0b6db9bd59e263218c6322c8bc7370a36b8a1127
Pablo Rodrigues Alexandre Silva <pablo0104@yahoo.com.br>
2023-01-16 20:41:25 -0300

    Api sem framework

---
+++ Index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+/**
+ * GET - Buscar um dado
+ * POST - Inserir um  dado
+ * PUT - Alterar um dado
+ * DELETE - Remover um dado
+ */
+
+const server = http.createServer((request, response) => {
+
+    if(request.url === "/users") {
+        if(request.method === "GET") {
+            return response.end("A aplicação esta no ar!")
+        }
+        if (request.method === "POST") {
+            return response.end("Chamada POST funcionando");
+        }
+    }
+});
+
+
+    server.listen(4000, () => console.log("Server is running on port 4000"))
